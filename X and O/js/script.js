0a921cd5478555105296ae542b6da8cf20b2e8b5
Viktor Iukhlin <iukhlin.v@gmail.com>
2020-08-03 23:37:02 +0300

    created new window

--- X and O/js/script.js
+++ X and O/js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const boardParent = document.querySelector('.board');
+const boardParent = document.querySelector('.board'),
+    scan = document.querySelector('.scan');
+let counter;
 
 function render() {
     for(let i = 0;i < 9;i++){
@@ -12,10 +14,13 @@ render();
 const allSectionsOfBoard = document.querySelectorAll('.board__section');
 
 allSectionsOfBoard.forEach(section => {
-    section.addEventListener('click', () => {
+    section.addEventListener('click', (e) => {
         section.classList.add('player');
+        console.log(e);
     },  {once:true});
 });
+scan.addEventListener
+ 
 
 
 // if(e.toElement.className == "board__section player"){
